33fef3eea9c6dd1f77f8f813bb386ee1ba7ae3d7
MOHAMEDHANIFAHARISH <mohamedhanifaharish1@gmail.com>
2024-07-05 18:06:25 +0530

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./home.css"
+
+const Social = () => {
+  return (
+   <div className="home__social">
+    <a href="https://github.com/MOHAMEDHANIFAHARISH" className="home__social-icon" target='_blank'><i class="uil uil-github"></i></a>
+    
+    <a href="https://www.linkedin.com/in/mohamed-hanifa-harish/" className="home__social-icon" target='_blank'><i class="uil uil-linkedin"></i></a>
+
+    <a href="https://wa.me/8248002316" className="home__social-icon" target='_blank'><i class="uil uil-whatsapp-alt"></i></a>
+
+
+   </div>
+  )
+}
+
+export default Social
