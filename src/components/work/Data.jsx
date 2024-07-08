9fafd60fa83345b37b51dd63490429fd3f76be8b
MOHAMEDHANIFAHARISH <mohamedhanifaharish1@gmail.com>
2024-07-09 00:54:03 +0530

    first commit

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,60 @@
+import Work1 from "../../assets/work1.png"
+import Work2 from "../../assets/work2.png"
+import Work3 from "../../assets/work3.png"
+import Work4 from "../../assets/work4.png"
+import Work5 from "../../assets/work5.png"
+import Work6 from "../../assets/work6.png"
+
+export const projectsData = [
+    {
+      id: 1,
+      image: Work1,
+      title: "Profile",
+      category: "web",
+    },
+    {
+      id: 2,
+      image: Work2,
+      title: "Calculator",
+      category: "app",
+    },
+    {
+      id: 3,
+      image: Work3,
+      title: "QR-Generate",
+      category: "app",
+    },
+    {
+      id: 4,
+      image: Work4,
+      title: "Interior Design",
+      category: "design",
+    },
+    {
+      id: 5,
+      image: Work5,
+      title: "UI/UX",
+      category: "web",
+    },
+    {
+      id: 6,
+      image: Work6,
+      title: "Web Design",
+      category: "web",
+    },
+  ];
+  
+  export const  projectsNav=[
+    {
+        name:'all',
+    },
+    {
+        name:'Web',
+    },
+    {
+        name:'app',
+    },
+    {
+        name:'design',
+    },
+  ]
