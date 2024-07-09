05514a28f8edaf01ffeb37a7a4347fb294f80608
MOHAMEDHANIFAHARISH <mohamedhanifaharish1@gmail.com>
2024-07-09 23:34:05 +0530

    first commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css"
 import AboutImg from"../../assets/profile.png";
-import CV from "../../assets/MohamedHanifaHarish.pdf";
+import CV from "../../assets/mohamedhanifaharish.pdf";
 import Info from './Info';
 
 
--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -3,20 +3,27 @@ import React from 'react'
 const Info = () => {
   return (
     <div className="about__info grid">
-    <div className="about__box">
+  
+   <div className="about__box">
+   <a href="#home" className='box__link'>
     <i class='bx bx-award about__icon'></i>
         <h3 className="about__title">Fresher</h3>
         <span className="about__subtitle">Looking for my new opportunity</span>
+        </a>
     </div>
     <div className="about__box">
+    <a href="#portfolio" className='box__link'>
     <i class='bx bx-briefcase about__icon' ></i>
         <h3 className="about__title">Completed</h3>
         <span className="about__subtitle">15+ Projects</span>
+        </a>
     </div>
     <div className="about__box">
+    <a href="#contact" className='box__link'>
     <i class='bx bx-support about__icon' ></i>
         <h3 className="about__title">Support</h3>
         <span className="about__subtitle">Online 24/7</span>
+        </a>
     </div>
     </div>
   )
